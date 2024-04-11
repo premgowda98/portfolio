2a0fd75e9ea099ce1792d7dd1124514f5a34f4b8
Prem Kumar G <premkumarmvks@gmail.com>
2024-04-11 15:58:26 +0530

    change: content

--- src/components/content/education.vue
+++ src/components/content/education.vue
@@ -1,4 +1,41 @@
 <template>
+    <div class="mt-3">
+        <div>
+            <h1 class="text-3xl font-bold">Certifications</h1>
+        </div>
+        <div class="mx-10 my-5 flex flex-col gap-5">
+            <div class="flex flex-col bg-[#B0F7C6] p-3 rounded-lg">
+                <div class="flex justify-between">
+                    <h3 class="font-bold text-lg">RabbitMQ Training Course</h3>
+                    <span class="font-bold text-lg">Feb 2024</span>
+                </div>
+                <div>
+                    <p>CloudAMQP</p>
+                </div>
+            </div>
+            <div class="flex flex-col bg-[#FFEED0] p-3 rounded-lg">
+                <div class="flex justify-between">
+                    <h3 class="font-bold text-lg">Introduction to Redis Data Structures</h3>
+                    <span class="font-bold text-lg">Nov 2023</span>
+                </div>
+                <div>
+                    <p>Redis</p>
+                </div>
+            </div>
+            <div class="flex flex-col bg-[#DDD1FA] p-3 rounded-lg">
+                <div class="flex justify-between">
+                    <h3 class="font-bold text-lg">Designing and Implementing a Data Science Solution on Azure</h3>
+                    <span class="font-bold text-lg">Feb 2023</span>
+                </div>
+                <div>
+                    <p>Microsoft</p>
+                </div>
+            </div>
+            <div class="flex justify-center">
+                <a href="https://www.linkedin.com/in/premgowda98/details/certifications/" target="_blank" class="bg-rose-500 rounded-lg p-2 text-md text-white font-bold">View More</a>
+            </div>
+        </div>
+    </div>
     <div>
         <div>
             <h1 class="text-3xl font-bold">Education</h1>
--- src/components/content/work.vue
+++ src/components/content/work.vue
@@ -3,36 +3,56 @@
         <div class="flex flex-col items-center justify-center my-6">
             <h2 class="text-2xl font-bold my-3">Insight Consultants (2022-24)</h2>
             <div class="flex flex-row justify-start gap-5 my-6">
-                <div class="bg-gray-800 w-1"></div>
-                <div class="w-96">
+                <div class="bg-gradient-to-b from-[rgba(131,58,180,1)]  to-[rgba(253,29,29,1)] w-1"></div>
+                <div class="md:w-96">
                     <div class="flex flex-col">
                         <div class="flex flex-row justify-between">
-                            <p class="text-lg font-bold">Software Enginnerr</p>
+                            <p class="text-xl font-bold">Software Engineer</p>
                             <p class="text-lg font-bold">2023-24</p>
+                        </div>
+                        <div class="flex gap-2 my-3">
+                            <div class="font-bold">Working Skills: </div>
+                            <div>
+                                <p>Python</p>
+                                <p>Vue3, Nuxt3</p>
+                                <p>G4A, SEO</p>
+                                <p>NGINX</p>
+                                <p>Docker</p>
+                                <p>Azure Pipelines</p>
+                            </div>
                         </div>
                         <div class="ml-8">
                             <ul class="list-disc">
-                                <li>sdfg</li>
-                                <li>sdfgsd</li>
-                                <li>gfsdg</li>
+                                <li>Developed and deployed a scalable digital fraud prevention solution, <b>Signetcloud.ai</b>, utilizing advanced facial verification, email validation, and document recognition models</li>
+                                <li>Designed and deployed an automated email campaign management tool, leveraging AI-powered content generation to improve customer outreach and engagement.</li>
+                                <li>Built an interactive chatbot for the leadership team using the RAG model, enabling senior leadership to better understand and address employee pain points based on the form filled by employees.</li>
                             </ul>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="flex flex-row justify-start gap-5 my-6">
-                <div class="bg-gray-800 w-1"></div>
-                <div class="w-96">
+                <div class="bg-gradient-to-b from-[rgba(29,253,179,1)]  to-[rgba(131,58,180,1)] w-1"></div>
+                <div class="md:w-96">
                     <div class="flex flex-col">
                         <div class="flex flex-row justify-between">
-                            <p class="text-lg font-bold">Finance Enginnerr</p>
+                            <p class="text-xl font-bold">Finance Engineer</p>
                             <p class="text-lg font-bold">2022-23</p>
+                        </div>
+                        <div class="flex gap-2 my-3">
+                            <div class="font-bold">Working Skills: </div>
+                            <div>
+                                <p>PowerBI</p>
+                                <p>Python (Pandas, Streamlit)</p>
+                                <p>UI Path</p>
+                                <p>Machine Learning</p>
+                            </div>
                         </div>
                         <div class="ml-8">
                             <ul class="list-disc">
-                                <li>dsfsg</li>
-                                <li>sfdgfg</li>
-                                <li>dfgfd</li>
+                                <li><b>Developed Impactful Power BI Dashboards:</b> Designed and built Power BI dashboards to track critical underwriting Key Performance Indicators (KPIs), enabling data-driven decision-making for the underwriting team.</li>
+                                <li><b>Automated Reconciliation Process:</b> Led the implementation of an automated reconciliation project using UiPath and Python, streamlining a previously manual-intensive process and improving efficiency</li>
+                                <li><b>Optimized Underwriting with Process Automation:</b> Spearheaded the development of a Python-based automated underwriting process, reducing the manual verification time from 10 minutes to less than 1 minute per customer.</li>
                             </ul>
                         </div>
                     </div>
